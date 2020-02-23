c6a90c291419900cb18ae1f59326ed516d47546c
Frank  D'Angelo <08fdange@gmail.com>
2020-02-23 18:44:31 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song 
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  
+  attr_reader :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+    
+  end
+  
+  def self.count 
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq 
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genre_count ={}
+    self.genres.each do |genre|
+      genre_count[genre] = [genre.count]
+    end
+    genre_count
+  end
+      
+  
+end
+  
+  
+    
+    
+  
