7af5a8e61e5b664db365ca57f3afd0cea4af821c
Frank  D'Angelo <08fdange@gmail.com>
2020-02-23 18:45:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,11 @@ class Song
   def self.genre_count
     genre_count ={}
     self.genres.each do |genre|
-      genre_count[genre] = [genre.count]
+      if genre_count[genre]
+        genre_count[genre] +=1 
+      else
+        genre_count[genre] = 1 
+      end
     end
     genre_count
   end
